df177d95ee6ac33c7382a017787e2ee3444517bf
Dillon Walker <dillon.walker@utexas.edu>
2013-11-12 20:30:13 -0600

    Bezier wrapping

--- animator/beziercurveevaluator.cpp
+++ animator/beziercurveevaluator.cpp
@@ -3,11 +3,17 @@
 
 using namespace std;
 
-void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts, 
+void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ctrlPts, 
 										 std::vector<Point>& ptvEvaluatedCurvePts, 
 										 const float& fAniLength, 
 										 const bool& bWrap) const {
 	ptvEvaluatedCurvePts.clear();
+  vector<Point> ptvCtrlPts = vector<Point>();
+  ptvCtrlPts.assign(ctrlPts.begin(), ctrlPts.end());
+  if (bWrap) {
+    Point a = ptvCtrlPts[0];
+    ptvCtrlPts.push_back(Point(a.x + fAniLength, a.y));
+  }
   int ctrlCount = ptvCtrlPts.size();
   int c = 0;
   double delta = 0.01;
@@ -20,7 +26,9 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 	  ptvEvaluatedCurvePts.push_back(Point(fAniLength, ptvCtrlPts[ctrlCount - 1].y));
     return;
   }
-  ptvEvaluatedCurvePts.push_back(Point(0.0, ptvCtrlPts[0].y));
+  if (!bWrap) {
+    ptvEvaluatedCurvePts.push_back(Point(0.0, ptvCtrlPts[0].y));
+  }
   ptvEvaluatedCurvePts.push_back(ptvCtrlPts[0]);
   int k = 0;
   for (int i = 0; i < ptvCtrlPts.size() - 3; i += (3 - c)) {
@@ -36,11 +44,22 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
       Point p012 = Point(p01.x * (1 - j) + p12.x * j, p01.y * (1 - j) + p12.y * j);
       Point p123 = Point(p12.x * (1 - j) + p23.x * j, p12.y * (1 - j) + p23.y * j);
       Point f = Point(p012.x * (1 - j) + p123.x * j, p012.y * (1 - j) + p123.y * j);
+      if (f.x > fAniLength) {
+        f.x -= fAniLength;
+      }
       ptvEvaluatedCurvePts.push_back(f);
     }
   }
   for (int i = k + 1; i < ptvCtrlPts.size(); i++) {
     ptvEvaluatedCurvePts.push_back(ptvCtrlPts[i]);
   }
-	ptvEvaluatedCurvePts.push_back(Point(fAniLength, ptvCtrlPts[ctrlCount - 1].y));
+  if (!bWrap) {
+    ptvEvaluatedCurvePts.push_back(Point(fAniLength, ptvCtrlPts[ctrlCount - 1].y));
+  } else {
+    if (k + 1 < ptvCtrlPts.size()) {
+      Point b = ptvEvaluatedCurvePts[ptvEvaluatedCurvePts.size()-2];
+      b = Point(b.x - fAniLength, b.y);
+      ptvEvaluatedCurvePts.insert(ptvEvaluatedCurvePts.begin(), b);
+    }
+  }
 }
