f458b6a24e9a6f5f359939de4275d8d4fbf38987
dwetterau <david.wetterau@gmail.com>
2013-11-30 20:37:19 -0600

    I think its working now

--- light/src/scene/camera.cpp
+++ light/src/scene/camera.cpp
@@ -23,7 +23,9 @@ Camera::rayThrough( double x, double y, ray &r, Vec3d skew)
 {
   x -= 0.5;
   y -= 0.5;
-  Vec3d dir = look + x * u + y * v + skew;
+  Vec3d dir = look + x * u + y * v;
+	dir.normalize();
+  dir += skew;
 	dir.normalize();
   r = ray( eye, dir, ray::VISIBILITY );
 }
